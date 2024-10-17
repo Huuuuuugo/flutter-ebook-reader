aa9dc66c2e81891ecb4937446d70fdb31530dd22
Hugo Messias <hugomessias9@gmail.com>
2024-10-17 17:03:41 -0300

    Atualizações para utilização do pacote 'Vocsy Epub Viewer' - Adicionadas dependências: vocsy_epub_viewer, permission_handler, dio, path_provider - Alterações em build.gradle e AndroidManifest.xml conforme documentação do pacote - Criado network_security_config.xml de acordo com as especificações do pacote - Implementada função getAndroidVersion em MainActivity.kt para suporte completo ao exemplo do pacote

--- android/app/src/main/kotlin/com/br/escribo/ebook_reader/ebook_reader/MainActivity.kt
+++ android/app/src/main/kotlin/com/br/escribo/ebook_reader/ebook_reader/MainActivity.kt
@@ -1,5 +1,28 @@
 package com.br.escribo.ebook_reader.ebook_reader
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.os.Build
 
-class MainActivity: FlutterActivity()
+// o exemplo de uso do Vocsy Epub Viewer disponibilizado pelos desenvolvedores faz uso do método "getAndroidVersion" 
+// para obter a versão do android do dispositivo e decidir a forma apropriada de acessar o armazenamento. Essa classe 
+// implementa esse método para que o exemplo possa ser propriamente usado no aplicativo.
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "my_channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getAndroidVersion" -> {
+                    result.success(Build.VERSION.RELEASE) // Send the Android version back
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
